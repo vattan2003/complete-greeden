c01bd54973376aa157d46f2e0af5761b9fd9ea26
vattan2003 <VATTANsince2003@gmail.com>
2023-12-23 13:10:37 +0530

    updated nav

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // menubar Activating
 
 function slide_nav() {
-    document.getElementById('slide_nav').style.marginLeft = "85%"
+    document.getElementById('slide_nav').style.marginLeft = "84%"
 
     // document.getElementById('slide_nav').classList.toggle('showSlideNavigation')
 }
